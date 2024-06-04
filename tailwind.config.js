19514747d156a36e75695abf56bd87c9e3375e79
Munish Kumar <send2munish@gmail.com>
2024-06-05 00:41:25 +0530

    Initial Setup - Configured Absolute Import - Installed TailwindCSS, React Router and React Icons - Added .prettierrc - Updated .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+		fontSize: {
+			base: "16px",
+		},
+		fontFamily: {
+			Averta: ["Averta"],
+		},
+	},
+	plugins: [],
+};
