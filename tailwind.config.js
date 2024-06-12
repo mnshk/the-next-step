b69975c175a81e73532581e2e70bb9d3dcc17cfc
Munish Kumar <send2munish@gmail.com>
2024-06-12 19:18:50 +0530

    Added fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: "class",
 	theme: {
-		extend: {},
-		fontSize: {
-			base: "16px",
+		extend: {
+			colors: {
+				accentPurple: {
+					100: "#F3DDE6",
+					500: "#291E29",
+					900: "#1B1016",
+				},
+				newPurple: {
+					50: "#871f78",
+					100: "#7a1c6c",
+					200: "#6c1960",
+					300: "#5f1654",
+					400: "#511348",
+					500: "#44103c",
+					600: "#360c30",
+					700: "#290924",
+					800: "#1b0618",
+					900: "#0d040c",
+				},
+			},
 		},
 		fontFamily: {
 			Averta: ["Averta"],
+			RobotoMono: ["Roboto Mono"],
+			Inter: ["Inter"],
+			DMSans: ["DM Sans"],
 		},
 	},
 	plugins: [],
-};
+}
